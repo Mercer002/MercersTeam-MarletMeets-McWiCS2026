f1db0109693585b2bb597968cc3e8555d39db7c9
cedricren123 <cedricrenard123@gmail.com>
2026-02-07 15:33:18 -0500

    3.1 ticket, Google maps api

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import StudentSignup from "./pages/StudentSignup";
 import Dashboard from "./pages/Dashboard";
+import MapTest from "./MapTest";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/signup">Student Signup</Link>
           <Link to="/dashboard">Dashboard</Link>
+          <Link to="/map-test">Map Test</Link>
+
         </nav>
       </header>
 
@@ -20,6 +23,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<StudentSignup />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/map-test" element={<MapTest />} />
         </Routes>
       </main>
     </div>
